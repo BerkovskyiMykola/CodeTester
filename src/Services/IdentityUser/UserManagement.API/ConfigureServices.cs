a1767d46045b6f7ffef2eea7c06175fd3c9de02d
Mykola Berkovskyi <nomvodafon@gmail.com>
2023-01-17 02:12:57 +0200

    add methods for each configuration

---
+++ src/Services/IdentityUser/UserManagement.API/ConfigureServices.cs
@@ -0,0 +1,151 @@
+﻿using DataAccess.Data;
+using DataAccess.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using UserManagement.API.Controllers;
+using UserManagement.API.EmailService;
+using UserManagement.API.Filters;
+
+namespace UserManagement.API;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddCustomSwagger(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "UserManagement HTTP API",
+                Version = "v1",
+                Description = "The UserManagement Service HTTP API"
+            });
+            options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.OAuth2,
+                Flows = new OpenApiOAuthFlows()
+                {
+                    Password = new OpenApiOAuthFlow()
+                    {
+                        TokenUrl = new Uri($"{configuration["IdentityUrlExternal"]}/connect/token"),
+                        Scopes = new Dictionary<string, string>()
+                        {
+                            { "usermanagement", "UserManagement API" }
+                        }
+                    }
+                }
+            });
+
+            options.OperationFilter<AuthorizeCheckOperationFilter>();
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        // prevent from mapping "sub" claim to nameidentifier.
+        JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+        var identityUrl = configuration.GetValue<string>("IdentityUrl");
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.Authority = configuration["IdentityUrl"];
+            options.RequireHttpsMetadata = false;
+            options.Audience = "usermanagement";
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddCustomConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions();
+        services.Configure<EmailConfiguration>(configuration.GetSection(nameof(EmailConfiguration)));
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.InvalidModelStateResponseFactory = context =>
+            {
+                var problemDetails = new ValidationProblemDetails(context.ModelState)
+                {
+                    Instance = context.HttpContext.Request.Path,
+                    Status = StatusCodes.Status400BadRequest,
+                    Detail = "Please refer to the errors property for additional details."
+                };
+
+                return new BadRequestObjectResult(problemDetails)
+                {
+                    ContentTypes = { "application/problem+json", "application/problem+xml" }
+                };
+            };
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration["ConnectionString"], sqlOptions =>
+            {
+                sqlOptions.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.GetName().Name);
+                sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+            });
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddCustomIdentity(this IServiceCollection services)
+    {
+        services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
+            options.SignIn.RequireConfirmedEmail = true;
+        })
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
+
+        services.Configure<DataProtectionTokenProviderOptions>(options =>
+        {
+            options.TokenLifespan = TimeSpan.FromHours(3);
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddCustomMvc(this IServiceCollection services)
+    {
+        // Add framework services.
+        services.AddControllers(options =>
+        {
+            options.Filters.Add(typeof(HttpGlobalExceptionFilter));
+        })
+        // Added for functional tests
+        .AddApplicationPart(typeof(AccountController).Assembly)
+        .AddJsonOptions(options => options.JsonSerializerOptions.WriteIndented = true);
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy",
+                builder => builder
+                .SetIsOriginAllowed((host) => true)
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+        });
+
+        return services;
+    }
+}
--- src/Services/IdentityUser/UserManagement.API/Program.cs
+++ src/Services/IdentityUser/UserManagement.API/Program.cs
@@ -1,12 +1,4 @@
-using DataAccess.Data;
-using DataAccess.Entities;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using System.IdentityModel.Tokens.Jwt;
 using UserManagement.API.EmailService;
-using UserManagement.API.Filters;
 
 namespace UserManagement.API;
 
@@ -16,107 +8,30 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        var connectionString = builder.Configuration["ConnectionString"];
-
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        {
-            options.UseSqlServer(connectionString, sqlOptions =>
-            {
-                sqlOptions.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.GetName().Name);
-                sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
-            });
-        });
+        var configuration = builder.Configuration;
 
         builder.Services
-            .AddIdentity<ApplicationUser, IdentityRole>(options =>
-            {
-                options.User.RequireUniqueEmail = true;
-                options.SignIn.RequireConfirmedEmail = true;
-            })
-            .AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddDefaultTokenProviders();
-
-        builder.Services.AddSwaggerGen(options =>
-        {
-            options.SwaggerDoc("v1", new OpenApiInfo
-            {
-                Title = "UserManagement HTTP API",
-                Version = "v1",
-                Description = "The UserManagement Service HTTP API"
-            });
-            options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-            {
-                Type = SecuritySchemeType.OAuth2,
-                Flows = new OpenApiOAuthFlows()
-                {
-                    Password = new OpenApiOAuthFlow()
-                    {
-                        TokenUrl = new Uri($"{builder.Configuration["IdentityUrlExternal"]}/connect/token"),
-                        Scopes = new Dictionary<string, string>()
-                        {
-                            { "usermanagement", "UserManagement API" }
-                        }
-                    }
-                }
-            });
-
-            options.OperationFilter<AuthorizeCheckOperationFilter>();
-        });
-
-        // prevent from mapping "sub" claim to nameidentifier.
-        JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
-
-        builder.Services
-            .AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.Authority = builder.Configuration["IdentityUrl"];
-                options.RequireHttpsMetadata = false;
-                options.Audience = "usermanagement";
-            });
-
-        builder.Services.Configure<EmailConfiguration>(
-            builder.Configuration.GetSection(nameof(EmailConfiguration)));
-
-        builder.Services.AddScoped<IEmailSender, EmailSender>();
-
-        builder.Services.Configure<DataProtectionTokenProviderOptions>(options =>
-        {
-            options.TokenLifespan = TimeSpan.FromHours(3);
-        });
-
-        builder.Services.AddAuthorization();
-
-        builder.Services.AddCors(options =>
-        {
-            options.AddPolicy("CorsPolicy",
-                builder => builder
-                .SetIsOriginAllowed((host) => true)
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .AllowCredentials());
-        });
-
-        builder.Services.AddControllers();
+            .AddCustomMvc()
+            .AddCustomDbContext(configuration)
+            .AddCustomDbContext(configuration)
+            .AddCustomIdentity()
+            .AddCustomSwagger(configuration)
+            .AddCustomAuthentication(configuration)
+            .AddCustomConfiguration(configuration)
+            .AddScoped<IEmailSender, EmailSender>();
 
         var app = builder.Build();
 
-        if (app.Environment.IsDevelopment())
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
         {
-            app.UseSwagger();
-            app.UseSwaggerUI(options =>
-            {
-                options.RoutePrefix = string.Empty;
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "UserManagement.API V1");
-                options.OAuthClientId("spa.client");
-                options.OAuthAppName("UserManagement Swagger UI");
-            });
-        }
+            options.RoutePrefix = string.Empty;
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "UserManagement.API V1");
+            options.OAuthClientId("spa.client");
+            options.OAuthAppName("UserManagement Swagger UI");
+        });
 
+        app.UseRouting();
         app.UseCors("CorsPolicy");
 
         app.UseAuthentication();
