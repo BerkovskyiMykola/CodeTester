687dd2b10e78658a79609f029b641c0e6f8891ff
Mykola Berkovskyi <nomvodafon@gmail.com>
2023-01-30 00:24:47 +0200

    add serilog for OcelotApiGw

---
+++ src/ApiGateways/OcelotApiGw/ConfigureServices.cs
@@ -0,0 +1,35 @@
+﻿using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+
+namespace OcelotApiGw;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        //services.AddAuthentication()
+        //    .AddJwtBearer("IdentityApiKey", x =>
+        //    {
+        //        x.Authority = configuration["IdentityUrl"];
+        //        x.RequireHttpsMetadata = false;
+        //        x.TokenValidationParameters = new TokenValidationParameters()
+        //        {
+        //            ValidAudiences = new[] { "usermanagment", "dictionary", "testing", "testingagg" }
+        //        };
+        //    });
+
+        return services;
+    }
+
+    public static IServiceCollection AddCustomOcelot(this IServiceCollection services, IConfiguration configuration)
+    {
+        var builder = services.AddOcelot();
+
+        builder.AddCacheManager(x =>
+         {
+             x.WithDictionaryHandle();
+         });
+
+        return services;
+    }
+}
