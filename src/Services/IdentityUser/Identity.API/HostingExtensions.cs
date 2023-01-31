a4cc046b54e6bb53768b6d14286550c6c9d86fd0
Mykola Berkovskyi <nomvodafon@gmail.com>
2023-02-01 01:01:41 +0200

    rewrite extentions

---
+++ src/Services/IdentityUser/Identity.API/HostingExtensions.cs
@@ -0,0 +1,261 @@
+﻿using DataAccess.Data;
+using DataAccess.Entities;
+using Duende.IdentityServer;
+using Duende.IdentityServer.EntityFramework.DbContexts;
+using HealthChecks.UI.Client;
+using Identity.API.Data;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Polly;
+
+namespace Identity.API;
+
+public static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        var configuration = builder.Configuration;
+
+        builder.Services
+            .AddCustomDbContext(configuration)
+            .AddCustomIdentity(configuration)
+            .AddCustomIdentityServer(configuration)
+            .AddCustomAuthentication(configuration)
+            .AddCustomConfiguration(configuration)
+            .AddCustomHealthCheck(configuration)
+            .AddRazorPages();
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        // Fix a problem with chrome. Chrome enabled a new feature "Cookies without SameSite must be secure", 
+        // the cookies should be expired from https, but in Microservices, the internal communication in aks and docker compose is http.
+        // To avoid this problem, the policy of cookies should be in Lax mode.
+        app.UseCookiePolicy(new CookiePolicyOptions { MinimumSameSitePolicy = SameSiteMode.Lax });
+
+        app.UseIdentityServer();
+
+        app.UseAuthorization();
+        app.MapRazorPages().RequireAuthorization();
+
+        app.MapHealthChecks("/hc", new HealthCheckOptions()
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+        app.MapHealthChecks("/liveness", new HealthCheckOptions
+        {
+            Predicate = r => r.Name.Contains("self")
+        });
+
+        return app;
+    }
+
+    public static WebApplication ApplyMigrations(this WebApplication app)
+    {
+        app.MigrateDbContext<ApplicationDbContext>((context, services) =>
+        {
+            var logger = services.GetRequiredService<ILogger<ApplicationDbContextSeed>>();
+            var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+            new ApplicationDbContextSeed()
+                .SeedAsync(context, logger, userManager, roleManager)
+                .Wait();
+        });
+
+        app.MigrateDbContext<PersistedGrantDbContext>((_, __) => { });
+
+        app.MigrateDbContext<ConfigurationDbContext>((context, services) =>
+        {
+            new ConfigurationDbContextSeed()
+                .SeedAsync(context, app.Configuration)
+                .Wait();
+        });
+
+        return app;
+    }
+
+
+    private static IServiceCollection AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration["ConnectionString"], sqlOptions =>
+            {
+                sqlOptions.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.GetName().Name);
+                sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+            });
+        });
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomIdentity(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomIdentityServer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+
+                // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
+                options.EmitStaticAudienceClaim = true;
+
+                options.IssuerUri = "null";
+                options.Authentication.CookieLifetime = TimeSpan.FromHours(2);
+            })
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder => builder.UseSqlServer(configuration["ConnectionString"],
+                    sqlServerOptionsAction: sqlOptions =>
+                    {
+                        sqlOptions.MigrationsAssembly(typeof(Program).Assembly.GetName().Name);
+                        sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                    });
+            })
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = builder => builder.UseSqlServer(configuration["ConnectionString"],
+                    sqlServerOptionsAction: sqlOptions =>
+                    {
+                        sqlOptions.MigrationsAssembly(typeof(Program).Assembly.GetName().Name);
+                        sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                    });
+            })
+            .AddAspNetIdentity<ApplicationUser>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddAuthentication()
+            .AddGoogle(options =>
+            {
+                options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                options.ClientId = configuration["Authentication:Google:ClientId"]!;
+                options.ClientSecret = configuration["Authentication:Google:ClientSecret"]!;
+            });
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomHealthCheck(this IServiceCollection services, IConfiguration configuration)
+    {
+        var hcBuilder = services.AddHealthChecks();
+
+        hcBuilder.AddCheck("self", () => HealthCheckResult.Healthy());
+
+        hcBuilder.AddSqlServer(
+            configuration["ConnectionString"]!,
+            name: "IdentityDB-check",
+            tags: new string[] { "IdentityDB" });
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions();
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.InvalidModelStateResponseFactory = context =>
+            {
+                var problemDetails = new ValidationProblemDetails(context.ModelState)
+                {
+                    Instance = context.HttpContext.Request.Path,
+                    Status = StatusCodes.Status400BadRequest,
+                    Detail = "Please refer to the errors property for additional details."
+                };
+
+                return new BadRequestObjectResult(problemDetails)
+                {
+                    ContentTypes = { "application/problem+json", "application/problem+xml" }
+                };
+            };
+        });
+
+        return services;
+    }
+
+    private static WebApplication MigrateDbContext<TContext>(this WebApplication host, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
+    {
+        using var scope = host.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        var logger = services.GetRequiredService<ILogger<TContext>>();
+        var context = services.GetRequiredService<TContext>();
+
+        try
+        {
+            logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
+
+            var retries = 10;
+            var retry = Policy.Handle<SqlException>()
+                .WaitAndRetry(
+                    retryCount: retries,
+                    sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    onRetry: (exception, timeSpan, retry, ctx) =>
+                    {
+                        logger.LogWarning(exception, "[{prefix}] Exception {ExceptionType} with message {Message} detected on attempt {retry} of {retries}", nameof(TContext), exception.GetType().Name, exception.Message, retry, retries);
+                    });
+
+            //if the sql server container is not created on run docker compose this
+            //migration can't fail for network related exception. The retry options for DbContext only 
+            //apply to transient exceptions
+            // Note that this is NOT applied when running some orchestrators (let the orchestrator to recreate the failing service)
+            retry.Execute(() => InvokeSeeder(seeder, context, services));
+
+            logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
+        }
+
+        return host;
+    }
+
+    private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context, IServiceProvider services)
+        where TContext : DbContext
+    {
+        context.Database.Migrate();
+        seeder(context, services);
+    }
+}
--- src/Services/IdentityUser/Identity.API/Program.cs
+++ src/Services/IdentityUser/Identity.API/Program.cs
@@ -1,12 +1,5 @@
 using Common.Logging;
-using DataAccess.Data;
-using DataAccess.Entities;
-using Duende.IdentityServer.EntityFramework.DbContexts;
-using HealthChecks.UI.Client;
 using Identity.API;
-using Identity.API.Data;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.AspNetCore.Identity;
 using Serilog;
 
 var configuration = GetConfiguration();
@@ -25,72 +18,16 @@ try
 
     builder.Host.UseSerilog();
 
-    builder.Services
-        .AddCustomDbContext(configuration)
-        .AddCustomIdentity(configuration)
-        .AddCustomIdentityServer(configuration)
-        .AddCustomAuthentication(configuration)
-        .AddCustomConfiguration(configuration)
-        .AddCustomHealthCheck(configuration)
-        .AddRazorPages();
-
-    var app = builder.Build();
-
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseDeveloperExceptionPage();
-    }
-    else
-    {
-        app.UseExceptionHandler("/Home/Error");
-    }
-
-    app.UseStaticFiles();
-    app.UseRouting();
-
-    // Fix a problem with chrome. Chrome enabled a new feature "Cookies without SameSite must be secure", 
-    // the cookies should be expired from https, but in Microservices, the internal communication in aks and docker compose is http.
-    // To avoid this problem, the policy of cookies should be in Lax mode.
-    app.UseCookiePolicy(new CookiePolicyOptions { MinimumSameSitePolicy = SameSiteMode.Lax });
-
-    app.UseIdentityServer();
-
-    app.UseAuthorization();
-    app.MapRazorPages().RequireAuthorization();
-
-    app.MapHealthChecks("/hc", new HealthCheckOptions()
-    {
-        Predicate = _ => true,
-        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-    });
-    app.MapHealthChecks("/liveness", new HealthCheckOptions
-    {
-        Predicate = r => r.Name.Contains("self")
-    });
+    var app = builder
+        .ConfigureServices()
+        .ConfigurePipeline();
 
     Log.Information("Applying migrations ({ApplicationContext})...", AppName);
 
-    app.MigrateDbContext<ApplicationDbContext>((context, services) =>
-    {
-        var logger = services.GetRequiredService<ILogger<ApplicationDbContextSeed>>();
-        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-
-        new ApplicationDbContextSeed()
-            .SeedAsync(context, logger, userManager, roleManager)
-            .Wait();
-    });
-
-    app.MigrateDbContext<PersistedGrantDbContext>((_, __) => { });
-
-    app.MigrateDbContext<ConfigurationDbContext>((context, services) =>
-    {
-        new ConfigurationDbContextSeed()
-            .SeedAsync(context, app.Configuration)
-            .Wait();
-    });
+    app.ApplyMigrations();
 
     Log.Information("Starting web host ({ApplicationContext})...", AppName);
+
     app.Run();
 
     return 0;
@@ -118,6 +55,6 @@ IConfiguration GetConfiguration()
 public partial class Program
 {
 
-    public static readonly string Namespace = typeof(ConfigureServices).Namespace!;
+    public static readonly string Namespace = typeof(HostingExtensions).Namespace!;
     public static readonly string AppName = Namespace;
 }
