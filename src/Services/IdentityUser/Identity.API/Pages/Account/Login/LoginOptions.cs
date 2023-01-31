0d52e1ee672ba81619a0274a648a9408a99477b9
Mykola Berkovskyi <nomvodafon@gmail.com>
2023-01-31 16:32:18 +0200

    configure login and logout options

--- src/Services/IdentityUser/Identity.API/Pages/Account/Login/LoginOptions.cs
+++ src/Services/IdentityUser/Identity.API/Pages/Account/Login/LoginOptions.cs
@@ -7,5 +7,5 @@ public class LoginOptions
     public static bool AllowLocalLogin = true;
     public static bool AllowRememberLogin = true;
     public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
-    public static string InvalidCredentialsErrorMessage = "Invalid username or password";
+    public static string InvalidCredentialsErrorMessage = "Invalid email or password";
 }
--- src/Services/IdentityUser/Identity.API/Pages/Account/Logout/LogoutOptions.cs
+++ src/Services/IdentityUser/Identity.API/Pages/Account/Logout/LogoutOptions.cs
@@ -5,5 +5,5 @@ namespace IdentityServerHost.Pages.Logout;
 public class LogoutOptions
 {
     public static bool ShowLogoutPrompt = true;
-    public static bool AutomaticRedirectAfterSignOut = false;
+    public static bool AutomaticRedirectAfterSignOut = true;
 }
