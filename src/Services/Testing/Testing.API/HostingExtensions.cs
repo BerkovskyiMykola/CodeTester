2fc3215ca323e931910e5ec9519c5ab0b9efa455
Mykola Berkovskyi <nomvodafon@gmail.com>
2023-02-02 02:48:48 +0200

    add base configurations

---
+++ src/Services/Testing/Testing.API/HostingExtensions.cs
@@ -0,0 +1,197 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using Testing.API.Filters;
+
+namespace Testing.API;
+
+public static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        var configuration = builder.Configuration;
+
+        builder.Services
+            .AddCustomSwagger(configuration)
+            .AddCustomAuthentication(configuration)
+            .AddCustomMvc(configuration)
+            .AddCustomHealthCheck(configuration)
+            .AddCustomConfiguration(configuration)
+            .AddCustomDbContext(configuration);
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Testing.API V1");
+            options.EnablePersistAuthorization();
+            options.OAuthClientId("testing-swagger");
+            options.OAuthScopes("openid", "testing", "roles");
+            options.OAuthUsePkce();
+        });
+
+        app.UseRouting();
+        app.UseCors("CorsPolicy");
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.MapHealthChecks("/hc", new HealthCheckOptions()
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+        app.MapHealthChecks("/liveness", new HealthCheckOptions
+        {
+            Predicate = r => r.Name.Contains("self")
+        });
+
+        return app;
+    }
+
+    public static WebApplication ApplyMigrations(this WebApplication app)
+    {
+
+
+        return app;
+    }
+
+
+    private static IServiceCollection AddCustomSwagger(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Testing HTTP API",
+                Version = "v1",
+                Description = "The Testing Service HTTP API"
+            });
+
+            var scheme = new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Name = "Authorization",
+                Flows = new OpenApiOAuthFlows
+                {
+                    AuthorizationCode = new OpenApiOAuthFlow
+                    {
+                        AuthorizationUrl = new Uri($"{configuration["IdentityUrlExternal"]}/connect/authorize"),
+                        TokenUrl = new Uri($"{configuration["IdentityUrlExternal"]}/connect/token")
+                    }
+                },
+                Type = SecuritySchemeType.OAuth2
+            };
+
+            options.AddSecurityDefinition("OAuth", scheme);
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference { Id = "OAuth", Type = ReferenceType.SecurityScheme }
+                    },
+                    new List<string> { }
+                }
+            });
+        });
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+        services
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.Authority = configuration["IdentityUrl"];
+                options.RequireHttpsMetadata = false;
+                options.Audience = "testing";
+            });
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomMvc(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Add framework services.
+        services.AddControllers(options =>
+        {
+            options.Filters.Add(typeof(HttpGlobalExceptionFilter));
+        })
+        // Added for functional tests
+        .AddJsonOptions(options => options.JsonSerializerOptions.WriteIndented = true);
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy",
+                builder => builder
+                .SetIsOriginAllowed((host) => true)
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+        });
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomHealthCheck(this IServiceCollection services, IConfiguration configuration)
+    {
+        var hcBuilder = services.AddHealthChecks();
+
+        hcBuilder.AddCheck("self", () => HealthCheckResult.Healthy());
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions();
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.InvalidModelStateResponseFactory = context =>
+            {
+                var problemDetails = new ValidationProblemDetails(context.ModelState)
+                {
+                    Instance = context.HttpContext.Request.Path,
+                    Status = StatusCodes.Status400BadRequest,
+                    Detail = "Please refer to the errors property for additional details."
+                };
+
+                return new BadRequestObjectResult(problemDetails)
+                {
+                    ContentTypes = { "application/problem+json", "application/problem+xml" }
+                };
+            };
+        });
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+
+
+        return services;
+    }
+}
