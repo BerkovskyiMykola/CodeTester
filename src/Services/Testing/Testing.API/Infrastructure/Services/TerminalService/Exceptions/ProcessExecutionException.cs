c0aee58d408ace62d23ebd4cd13f5e0cd323b463
Mykola Berkovskyi <nomvodafon@gmail.com>
2023-03-04 03:19:41 +0200

    rename exception

---
+++ src/Services/Testing/Testing.API/Infrastructure/Services/TerminalService/Exceptions/ProcessExecutionException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace Testing.API.Infrastructure.Services.TerminalService.Exceptions;
+
+public class ProcessExecutionException : Exception
+{
+    public ProcessExecutionException()
+    {
+    }
+
+    public ProcessExecutionException(string? message) : base(message)
+    {
+    }
+
+    public ProcessExecutionException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected ProcessExecutionException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
