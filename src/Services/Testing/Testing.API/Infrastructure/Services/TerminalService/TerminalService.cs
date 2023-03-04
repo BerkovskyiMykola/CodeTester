94b75c152369fdaa0844f728bfb93daaa4c3e048
Mykola Berkovskyi <nomvodafon@gmail.com>
2023-03-04 03:18:07 +0200

    upgrate terminal service

--- src/Services/Testing/Testing.API/Infrastructure/Services/TerminalService/TerminalService.cs
+++ src/Services/Testing/Testing.API/Infrastructure/Services/TerminalService/TerminalService.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using Testing.API.Infrastructure.Services.TerminalService.Exceptions;
 using Testing.API.Infrastructure.Services.TerminalService.Models;
 
 namespace Testing.API.Infrastructure.Services.TerminalService;
@@ -22,33 +24,58 @@ public class TerminalService : ITerminalService
             throw new ArgumentException("Command should have at least one element");
         }
 
-        var cmd = new Process();
-        cmd.StartInfo.FileName = "/bin/bash";
-        cmd.StartInfo.RedirectStandardError = true;
-        cmd.StartInfo.RedirectStandardInput = true;
-        cmd.StartInfo.RedirectStandardOutput = true;
-        cmd.StartInfo.CreateNoWindow = true;
-        cmd.StartInfo.UseShellExecute = false;
-        cmd.Start();
-
-        foreach (var line in command)
+        try
         {
-            cmd.StandardInput.Write(line);
-            cmd.StandardInput.Write(" ");
+            var cmd = new Process();
+            cmd.StartInfo.FileName = "/bin/bash";
+            cmd.StartInfo.RedirectStandardError = true;
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            foreach (var line in command)
+            {
+                cmd.StandardInput.Write(line);
+                cmd.StandardInput.Write(" ");
+            }
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            if (!cmd.WaitForExit(timeout))
+            {
+                try
+                {
+                    cmd.Kill();
+                }
+                catch (InvalidOperationException)
+                {
+                    cmd.WaitForExit();
+
+                    return new TerminalOutput(
+                        await cmd.StandardOutput.ReadToEndAsync(),
+                        await cmd.StandardError.ReadToEndAsync(),
+                        (int)stopWatch.Elapsed.TotalSeconds,
+                        cmd.ExitCode);
+                }
+
+                throw new ProcessExecutionTimeoutException("The process takes longer to execute than the time limit set");
+            }
+            stopWatch.Stop();
+
+            return new TerminalOutput(
+                await cmd.StandardOutput.ReadToEndAsync(),
+                await cmd.StandardError.ReadToEndAsync(),
+                (int)stopWatch.Elapsed.TotalSeconds,
+                cmd.ExitCode);
         }
-        cmd.StandardInput.Flush();
-        cmd.StandardInput.Close();
-
-        var stopWatch = new Stopwatch();
-
-        stopWatch.Start();
-        cmd.WaitForExit(timeout);
-        stopWatch.Stop();
-
-        return new TerminalOutput(
-            await cmd.StandardOutput.ReadToEndAsync(),
-            await cmd.StandardError.ReadToEndAsync(),
-            (int)stopWatch.Elapsed.TotalSeconds,
-            cmd.ExitCode);
+        catch (Exception e)
+        {
+            throw new CommandExecutionException("Fatal error for command " + string.Join(" ", command) + " : " + e.Message);
+        }
     }
 }
