762163e82e619790a1654136aeb0a46c30b847a1
Mykola Berkovskyi <nomvodafon@gmail.com>
2023-03-06 17:57:15 +0200

    init execution

---
+++ src/Services/Testing/Testing.API/Infrastructure/Services/TestExecutionSerivce/Executions/Execution.cs
@@ -0,0 +1,61 @@
+﻿using Testing.API.Infrastructure.Services.TestExecutionSerivce.Executions.Models;
+
+namespace Testing.API.Infrastructure.Services.TestExecutionSerivce.Executions;
+
+public class Execution
+{
+    public Guid Id { get; init; }
+    private readonly ExecutionOptions _options;
+
+    private readonly string _executionPath;
+
+    public Execution(ExecutionOptions options)
+    {
+        Id = Guid.NewGuid();
+        _options = options;
+
+        _executionPath = Path.Combine(options.ExecutionsFolderPath, Id.ToString());
+    }
+
+    public void CreateExecutionDirectory()
+    {
+        Directory.CreateDirectory(_executionPath);
+
+        CopyTemplateFilesToExecutionDirectory();
+        PrepareDockerFileToExecution();
+        PrepareExecutionFileToExecution();
+    }
+
+    private void CopyTemplateFilesToExecutionDirectory()
+    {
+        var source = new DirectoryInfo(_options.TemplatePath);
+        var target = new DirectoryInfo(_executionPath);
+
+        foreach (FileInfo fi in source.GetFiles())
+        {
+            fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+        }
+    }
+
+    private void PrepareDockerFileToExecution()
+    {
+        var dockerFile = Path.Combine(_executionPath, _options.TemplateDockerfileName);
+        var content = File.ReadAllText(dockerFile).Replace("{timeLimit}", _options.TimeLimit.ToString());
+        File.WriteAllText(dockerFile, content);
+    }
+
+    private void PrepareExecutionFileToExecution()
+    {
+        var executionFile = Path.Combine(_executionPath, _options.TemplateDockerfileName);
+        var content = File.ReadAllText(executionFile)
+            .Replace("{code}", _options.Code)
+            .Replace("{tests}", _options.Tests);
+
+        File.WriteAllText(executionFile, content);
+    }
+
+    public void DeleteExecutionDirectory()
+    {
+        Directory.Delete(_executionPath, true);
+    }
+}
