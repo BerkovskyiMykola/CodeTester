60ea780cf59a52c2e630c8dc486cddd0ee58ce85
Mykola Berkovskyi <nomvodafon@gmail.com>
2023-03-12 03:00:01 +0200

    add PaginationResult

---
+++ src/Services/Testing/Testing.API/Infrastructure/Models/PaginationResult.cs
@@ -0,0 +1,23 @@
+﻿namespace Testing.API.Infrastructure.Models;
+
+public record PaginationResult<T>
+{
+    public int CurrentPage { get; }
+    public int TotalPages { get; }
+    public int PageSize { get; }
+    public int TotalCount { get; }
+
+    public bool HasPrevious => CurrentPage > 1;
+    public bool HasNext => CurrentPage < TotalPages;
+
+    public ICollection<T> Rows { get; }
+
+    public PaginationResult(List<T> items, int count, int pageNumber, int pageSize)
+    {
+        Rows = items;
+        TotalCount = count;
+        PageSize = pageSize;
+        CurrentPage = pageNumber;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+    }
+}
