17a57a4bafa4b20fd685ca926dfbe6499d4fdc16
Mykola Berkovskyi <nomvodafon@gmail.com>
2023-03-13 19:41:18 +0200

    remove using

--- src/Services/Testing/Testing.API/Infrastructure/Services/TerminalService/TerminalService.cs
+++ src/Services/Testing/Testing.API/Infrastructure/Services/TerminalService/TerminalService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Testing.API.Infrastructure.Models;
 using Testing.API.Infrastructure.Services.TerminalService.Exceptions;
 
